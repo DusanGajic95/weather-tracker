2388415147d0f29080d7ced76cca6168038cf51a
AleksandarKacar <kacar95@gmail.com>
2024-04-24 19:43:53 +0200

    first comit

---
+++ dan-8-zadatak-1/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CityWeatherList from "./component/CityWeatherList";
+
+function App() {
+  return (
+    <div className="App">
+      <CityWeatherList />
+    </div>
+  );
+}
+
+export default App;
---
+++ dan-8-zadatak-1/src/component/City.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const City: FC = () => {
+  return (
+    <tr>
+      <td>{}</td>
+      <td>dsadas</td>
+      <td>dsadasd </td>
+    </tr>
+  );
+};
+export default City;
